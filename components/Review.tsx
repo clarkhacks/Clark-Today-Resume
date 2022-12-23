0eac9336f1d9aa6812bc33ee647634f7f8cffae9
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2022-12-22 20:27:50 -0600

    Init

---
+++ components/Review.tsx
@@ -0,0 +1,3 @@
+import * as React from 'react'
+import styles from './styles.module.css'
+// review form with emojis left to right
---
+++ components/Tracking.tsx
@@ -0,0 +1,5 @@
+export const Tracking = () => {
+    return (
+        <script async defer data-website-id="fde93121-bb78-4430-ba78-d90005e4f77c" src="https://umami.soup.cool/umami.js"></script>
+    )
+    }
